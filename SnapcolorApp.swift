119c259e8dc4b39a7f057839a6c21d9c33f0fe2e
nooralotibi <116544357+nooralotibi@users.noreply.github.com>
2023-01-12 07:26:42 +0300

    Add files via upload

---
+++ SnapcolorApp.swift
@@ -0,0 +1,18 @@
+//
+//  SnapcolorApp.swift
+//  Snapcolor
+//
+//  Created by Shifa Alfaisal on 11/06/1444 AH.
+//
+
+import SwiftUI
+
+
+@main
+struct SnapcolorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Splash()
+        }
+    }
+}
